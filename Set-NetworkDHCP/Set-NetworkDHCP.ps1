3aeb6f525e4f337d266992c91bd4b1faea959e80
Cédric RIBIER <cedricribier.plf@gmail.com>
2026-05-14 16:08:40 +0200

    Create Set-NetworkDHCP.ps1

---
+++ Set-NetworkDHCP/Set-NetworkDHCP.ps1
@@ -0,0 +1,5 @@
+# Active le mode DHCP sur toutes les interfaces réseau actives
+
+Get-NetIPInterface | Where-Object { $_.Dhcp -eq "Disabled" -and $_.InterfaceOperationalStatus -eq "Up" } | ForEach-Object {
+    Set-NetIPInterface -InterfaceIndex $_.InterfaceIndex -Dhcp Enabled
+}
